43fff25d924c85963f14d84286821190e586ae60
rahul8796roy <124188410+rahul8796roy@users.noreply.github.com>
2024-09-04 10:42:51 +0000

    container config done

---
+++ JS/conversion.js
@@ -0,0 +1,71 @@
+/*
+let score = 33
+
+console.log(typeof score)
+console.log(typeof(score)) // output => number, number 
+*/
+
+/*
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof(score)) // output => String, String 
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber) // Output => Number 
+console.log(valueInNumber) // Output => "33abc" => NaN 
+*/
+
+
+/*
+let score = undefined
+
+console.log(typeof score)
+console.log(typeof(score)) // output => String, String 
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber) // Output => Number 
+console.log(valueInNumber) // Output => "undefined" => NaN 
+*/
+
+
+/*
+let score = null
+
+console.log(typeof score)
+console.log(typeof(score)) // output => Object, Object
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber) // Output => Number 
+console.log(valueInNumber) // Output => "null" => 0
+*/
+
+
+/*
+let score = true
+
+console.log(typeof score)
+console.log(typeof(score)) // output => boolean, boolean
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber) // Output => Number 
+console.log(valueInNumber) // Output => true => 1 and False => 0
+*/
+
+
+/*
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);              // Output => True 
+*/
+
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)              // Output => 33
+console.log(typeof stringNumber)      // Output => string
+
+
---
+++ JS/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as new version
+
+// alert(3 + 3)
+
+
+console.log("Rahul Kumar Roy")
+
+console.log(5+5)
+
+let name = "Rahul"
+let age = "24"
+
+
+// number => 2 to power 53
+// bigint
+// string => '' / " "
+// boolean => true / false 
+// null => standlone value
+// undefined => 
+// symbol => unique 
+
+// object 
+
+console.log(typeof null);
+
